835c62590ac94ac3a4e8b3e2eda95d0b598194dd
matteopolak <matteopolak@hotmail.com>
2024-11-06 13:37:31 -0500

    feat: reduce input requirements for wasm

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -4,8 +4,8 @@ use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen(js_name = fromHtml)]
 #[must_use]
-pub fn from_html(html: &str) -> String {
-	let courses = uo2ics_core::course::parse_from_buf(html.as_bytes());
+pub fn from_html(html: &[u8]) -> String {
+	let courses = uo2ics_core::course::parse_from_buf(html);
 	let calendar = uo2ics_core::create_calendar(courses);
 
 	calendar.to_string()
